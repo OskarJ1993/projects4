72fd1a7dedaf725de642e7e6ca53edcee352ebe4
Oskar Johansson <oskarjohansson@HP-EliteBook-840-G3-5ce9c124.localdomain>
2023-07-23 23:22:59 +0200

    fix database problem

--- project4/settings.py
+++ project4/settings.py
@@ -79,18 +79,12 @@ import dj_database_url
 
 DATABASES = {
     'default': 
-        dj_database_url.parse(os.environ.get('DATABASE_URL')
+        dj_database_url.parse(os.environ.get('DATABASE_URL'))
     
 }
 
 # render database
 
-import dj_database_url
-
-# DATABASES = {
-#     'default' : dj_database_url.parse(env('DATABASE_URL'))
-
-# }
 
 
 
