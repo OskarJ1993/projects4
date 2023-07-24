cb674f00b1041528e375e92c5c4a58e00ae9c020
Oskar Johansson <oskarjohansson@HP-EliteBook-840-G3-5ce9c124.localdomain>
2023-07-24 03:06:44 +0200

    debugging

--- project4/settings.py
+++ project4/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '7mu46wg3^(2c23kq0lp5s=u8(h+j(&7z=wu-!_+dlf%_58-6&u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
